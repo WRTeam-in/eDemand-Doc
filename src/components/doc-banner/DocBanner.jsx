55f383c84271d60eff19aa225a1b1227337399c2
Yash Rajput <yashwrteam89@gmail.com>
2026-02-11 16:20:39 +0530

    update link

--- src/components/doc-banner/DocBanner.jsx
+++ src/components/doc-banner/DocBanner.jsx
@@ -432,7 +432,7 @@ const DocBanner = () => {
 
             <div className={styles.bannerAction}>
                 <Link
-                    href="https://api.whatsapp.com/send?phone=918200323468&text=Hello%20Jignesh%2C%20I%20want%2[…]0more%20about%20the%20installation%20service%20for%20eDemand"
+                    href="https://api.whatsapp.com/send?phone=918200323468&text=Hello%20Jignesh%2C%20I%20want%20to%20know%20more%20about%20the%20installation%20service%20for%20eDemand"
                     target="_blank"
                     rel="noopener noreferrer"
                     className={styles.btnPrimary}
